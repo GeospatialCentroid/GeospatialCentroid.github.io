08b3b80e9d60741f22eaf51392903946206c0440
kevinworthington <kworthin@gmail.com>
2025-09-19 10:22:18 -0600

    added UX improments

--- js/filter.js
+++ js/filter.js
@@ -714,19 +714,19 @@ class Filter_Manager {
           var title=m["title"]
           var description=m["short_desc"]
 
-                html += '<div class="flip-card">'
+                html += '<div class="flip-card" tabindex="0" role="button" aria-pressed="false">'
                 html += ' <div class="flip-card-inner" title="'+m["image"]+'">'
                 html += '<div class="flip-card-front">'
-                html +='<div class="flip-card-bottom-text card_click">'+title+'</div>'
+                html +='<div class="flip-card-title-text flip-card-bottom-text card_click">'+title+'</div>'
                 html += ' </div>'
                 html += '<div class="flip-card-back">'
-
-                html+=description
+               html +='<div class="flip-card-title-text flip-card-top-text">'+title+'</div>'
+                html+= '<div class="flip-card-desc">'+description+'</div>'
                 if(m["github_url"]!=""){
-                    html+='<div class="icon left_icon"><a href="'+s["github_url"]+'" target="_black"><img style="width:15px;" src="images/github-mark-white.svg"</a></div>'
+                    html+='<div class="icon left_icon"><a href="'+s["github_url"]+'" target="_black"><svg width="16" height="16" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg"><path transform="scale(.17)"  d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" /></svg></a></div>'
                 }
                 if(m["project_url"]!=""){
-                    html+='<div class="icon right_icon"><a href="'+s["project_url"]+'" target="_black"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/></svg></a></div>'
+                    html+='<div class="icon right_icon"><a href="'+s["project_url"]+'" target="_black"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#105456" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/></svg></a></div>'
                 }
                 html += ' </div>'
                 html += ' </div>'
@@ -751,7 +751,26 @@ class Filter_Manager {
             }
           });
         });
-//
+        // tab support - not yet working
+        document.querySelectorAll('.flip-card').forEach(card => {
+//          card.addEventListener('keydown', e => {
+//            if (e.key === 'Enter' || e.key === ' ' /* space */) {
+//                 card.classList.add('is-flipped');
+//            }
+//          });
+          card.addEventListener('focusin', e => {
+             card.classList.add('is-flipped');
+          });
+           card.addEventListener('focusout', (e) => {
+            // e.relatedTarget is the element receiving focus next
+            const next = e.relatedTarget;
+
+            // If next is NOT inside the current .flip-card, focus really left
+            if (!card.contains(next)) {
+               card.classList.remove('is-flipped');
+            }
+          });
+        });
 //        // inspired by https://github.com/TylerPottsDev/card-flip
         cards.forEach(card => {
             card.addEventListener("click", function (e) {
